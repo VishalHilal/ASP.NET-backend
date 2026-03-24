e3bcf543b8a16274b46cd0b08667a62a1823952a
VishalHilal <vishalschool24@gmail.com>
2026-03-24 21:38:54 +0530

    route testing

--- Program.cs
+++ Program.cs
@@ -1,10 +1,47 @@
 var builder = WebApplication.CreateBuilder(args);
-
 var app = builder.Build();
 
-app.MapGet("/", ()=>{
-		return new{
-		message = "test backend"
-		};
-		});
+app.MapGet("/", () => {
+    return new {
+        message = "test backend"
+    };
+});
+
+
+app.MapGet("/user/{id}", (int id) => {
+    return new {
+        userId = id,
+        name = "Vishal"
+    };
+});
+
+app.MapGet("/search", (string name) => {
+    return new {
+        query = name,
+        result = $"Searching for {name}"
+    };
+});
+
+app.MapPost("/user", (User user) => {
+    return new {
+        message = "User created successfully",
+        data = user
+    };
+});
+
+app.MapPut("/user/{id}", (int id, User user) => {
+    return new {
+        message = $"User {id} updated",
+        updatedData = user
+    };
+});
+
+app.MapDelete("/user/{id}", (int id) => {
+    return new {
+        message = $"User {id} deleted"
+    };
+});
+
 app.Run();
+
+public record User(int Id, string Name, string Email);
